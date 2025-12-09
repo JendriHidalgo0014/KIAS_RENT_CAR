af5d9ebde854564540d401be87defdf4eacec4f8
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-08 21:19:03 -0400

    Se creó RepositoryModule que vincula implementaciones con interfaces de repositorios, usa @Binds para inyección eficiente y facilita testing con implementaciones mock.

--- app/src/main/java/edu/ucne/kias_rent_car/di/RepositoryModule.kt
+++ app/src/main/java/edu/ucne/kias_rent_car/di/RepositoryModule.kt
@@ -4,8 +4,14 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.kias_rent_car.data.repository.MensajeRepositoryImpl
+import edu.ucne.kias_rent_car.data.repository.ReservacionRepositoryImpl
+import edu.ucne.kias_rent_car.data.repository.UbicacionRepositoryImpl
 import edu.ucne.kias_rent_car.data.repository.VehicleRepositoryImpl
 import edu.ucne.kias_rent_car.data.repository.UsuarioRepositoryImpl
+import edu.ucne.kias_rent_car.domain.repository.MensajeRepository
+import edu.ucne.kias_rent_car.domain.repository.ReservacionRepository
+import edu.ucne.kias_rent_car.domain.repository.UbicacionRepository
 import edu.ucne.kias_rent_car.domain.repository.VehicleRepository
 import edu.ucne.kias_rent_car.domain.repository.UsuarioRepository
 import javax.inject.Singleton
@@ -13,16 +19,23 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindUsuarioRepository(impl: UsuarioRepositoryImpl): UsuarioRepository
 
     @Binds
     @Singleton
-    abstract fun bindVehicleRepository(
-        impl: VehicleRepositoryImpl
-    ): VehicleRepository
+    abstract fun bindVehicleRepository(impl: VehicleRepositoryImpl): VehicleRepository
 
     @Binds
     @Singleton
-    abstract fun bindUsuarioRepository(
-        impl: UsuarioRepositoryImpl
-    ): UsuarioRepository
+    abstract fun bindUbicacionRepository(impl: UbicacionRepositoryImpl): UbicacionRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindReservacionRepository(impl: ReservacionRepositoryImpl): ReservacionRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindMensajeRepository(impl: MensajeRepositoryImpl): MensajeRepository
 }
