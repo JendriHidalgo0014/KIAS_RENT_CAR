8170c21175273ed65d7381ad285db8061c0768bb
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-08 21:57:55 -0400

    Se creó GetAllVehiclesUseCase para obtener todos los vehiculos.

--- app/src/main/java/edu/ucne/kias_rent_car/domain/usecase/Vehicle/GetAllVehiclesUseCase.kt
+++ app/src/main/java/edu/ucne/kias_rent_car/domain/usecase/Vehicle/GetAllVehiclesUseCase.kt
@@ -5,8 +5,10 @@ import edu.ucne.kias_rent_car.domain.repository.VehicleRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetAvailableVehiclesUseCase @Inject constructor(
-    private val repo: VehicleRepository
+class GetAllVehiclesUseCase @Inject constructor(
+    private val repository: VehicleRepository
 ) {
-    operator fun invoke(): Flow<List<Vehicle>> = repo.observeAvailableVehicles()
+    suspend operator fun invoke(): Flow<List<Vehicle>> {
+        return repository.observeAvailableVehicles()
+    }
 }
