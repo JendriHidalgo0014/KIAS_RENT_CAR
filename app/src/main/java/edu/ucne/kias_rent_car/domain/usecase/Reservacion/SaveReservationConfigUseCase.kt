4b54e03696ec16e67f7d6905f96f194eba12acdb
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-08 21:47:59 -0400

    Se creó SaveReservationConfigUseCase para guardar la configuracion de las reservas.

---
+++ app/src/main/java/edu/ucne/kias_rent_car/domain/usecase/Reservacion/SaveReservationConfigUseCase.kt
@@ -0,0 +1,13 @@
+package edu.ucne.kias_rent_car.domain.usecase.Reservacion
+
+import edu.ucne.kias_rent_car.domain.model.ReservationConfig
+import edu.ucne.kias_rent_car.domain.repository.ReservacionRepository
+import javax.inject.Inject
+
+class SaveReservationConfigUseCase @Inject constructor(
+    private val repository: ReservacionRepository
+) {
+    suspend operator fun invoke(config: ReservationConfig) {
+        repository.saveReservationConfig(config)
+    }
+}
