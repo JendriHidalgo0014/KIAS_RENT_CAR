f60dbc0c6bb03c99f032b33ed4ea5fecaa6ee840
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-08 21:53:11 -0400

    Se creó GetUsuarioLogueadoUseCase para obtener el usuario que este logueado en el momento.

---
+++ app/src/main/java/edu/ucne/kias_rent_car/domain/usecase/Usuario/GetUsuarioLogueadoUseCase.kt
@@ -0,0 +1,13 @@
+package edu.ucne.kias_rent_car.domain.usecase.Usuario
+
+import edu.ucne.kias_rent_car.domain.model.Usuario
+import edu.ucne.kias_rent_car.domain.repository.UsuarioRepository
+import javax.inject.Inject
+
+class GetUsuarioLogueadoUseCase @Inject constructor(
+    private val repository: UsuarioRepository
+) {
+    suspend operator fun invoke(): Usuario? {
+        return repository.getUsuarioLogueado()
+    }
+}
