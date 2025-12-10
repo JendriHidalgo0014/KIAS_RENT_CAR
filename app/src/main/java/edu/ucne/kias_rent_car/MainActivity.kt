27099f1bdb329fc17bd8d023af604e007f13b2c2
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-10 03:00:36 -0400

    Se modifico para las nuevas rutas.

--- app/src/main/java/edu/ucne/kias_rent_car/MainActivity.kt
+++ app/src/main/java/edu/ucne/kias_rent_car/MainActivity.kt
@@ -3,17 +3,26 @@ package edu.ucne.kias_rent_car
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.kias_rent_car.presentation.AppNavigation
-
+import edu.ucne.kias_rent_car.navigation.AppNavigation
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MaterialTheme() {
-                AppNavigation()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
+                }
             }
         }
     }
