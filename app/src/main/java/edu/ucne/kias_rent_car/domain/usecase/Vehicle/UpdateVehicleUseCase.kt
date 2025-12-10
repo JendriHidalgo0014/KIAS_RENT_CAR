91e1a482784292ed55b904e89bcb548bfb4d8448
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-10 02:50:09 -0400

    Se modifico la logica.

--- app/src/main/java/edu/ucne/kias_rent_car/domain/usecase/Vehicle/UpdateVehicleUseCase.kt
+++ app/src/main/java/edu/ucne/kias_rent_car/domain/usecase/Vehicle/UpdateVehicleUseCase.kt
@@ -1,5 +1,6 @@
 package edu.ucne.kias_rent_car.domain.usecase.Vehicle
 
+import edu.ucne.kias_rent_car.data.remote.Resource
 import edu.ucne.kias_rent_car.data.repository.VehicleRepositoryImpl
 import javax.inject.Inject
 
@@ -10,9 +11,21 @@ class UpdateVehicleUseCase @Inject constructor(
         id: String,
         modelo: String,
         descripcion: String,
+        categoria: String,
+        asientos: Int,
+        transmision: String,
         precioPorDia: Double,
         imagenUrl: String
-    ) {
-        repository.updateVehicle(id, modelo, descripcion, precioPorDia, imagenUrl)
+    ): Resource<Unit> {
+        return repository.updateVehicle(
+            id = id,
+            modelo = modelo,
+            descripcion = descripcion,
+            categoria = categoria,
+            asientos = asientos,
+            transmision = transmision,
+            precioPorDia = precioPorDia,
+            imagenUrl = imagenUrl
+        )
     }
 }
