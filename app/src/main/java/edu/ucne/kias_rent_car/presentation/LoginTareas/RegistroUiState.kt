6da83d1e428a75623753dfd59816067965cb31eb
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-11 16:02:37 -0400

    Corrigiendo Issues de sonar.

--- app/src/main/java/edu/ucne/kias_rent_car/presentation/LoginTareas/RegistroUiState.kt
+++ app/src/main/java/edu/ucne/kias_rent_car/presentation/LoginTareas/RegistroUiState.kt
@@ -3,20 +3,12 @@ package edu.ucne.kias_rent_car.presentation.LoginTareas
 data class RegistroUiState(
     val nombre: String = "",
     val email: String = "",
-    val emailError: String? = null,
     val telefono: String = "",
     val password: String = "",
     val confirmPassword: String = "",
     val passwordVisible: Boolean = false,
     val isLoading: Boolean = false,
-    val error: String? = null,
-    val registroExitoso: Boolean = false
-) {
-    fun esFormularioValido(): Boolean {
-        return nombre.isNotBlank() &&
-                email.isNotBlank() &&
-                emailError == null &&
-                password.length >= 4 &&
-                password == confirmPassword
-    }
-}
+    val registroExitoso: Boolean = false,
+    val userMessage: String? = null,
+    val emailError: String? = null
+)
