bfd85754e27709eb0101db67dd23ff76ae47efd2
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-12 00:02:01 -0400

    Corrigiendo Issues de Sonar.

--- app/src/main/java/edu/ucne/kias_rent_car/presentation/ClienteTareas/BookingsTareas/BookingsUiEvent.kt
+++ app/src/main/java/edu/ucne/kias_rent_car/presentation/ClienteTareas/BookingsTareas/BookingsUiEvent.kt
@@ -1,6 +1,10 @@
 package edu.ucne.kias_rent_car.presentation.ClienteTareas.BookingsTareas
 
-sealed class BookingsEvent {
-    data class FilterChanged(val filter: BookingFilter) : BookingsEvent()
-    data object Refresh : BookingsEvent()
+sealed interface BookingsUiEvent {
+    data class FilterChanged(val filter: BookingFilter) : BookingsUiEvent
+    data class NavigateToDetail(val bookingId: Int) : BookingsUiEvent
+    object Refresh : BookingsUiEvent
+    object NavigateToHome : BookingsUiEvent
+    object NavigateToSupport : BookingsUiEvent
+    object NavigateToProfile : BookingsUiEvent
 }
