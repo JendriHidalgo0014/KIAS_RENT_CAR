d609c8e34014f8db9bc02c5576f1d9d426dbb12e
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-12-11 23:58:36 -0400

    Corrigiendo Issues de Sonar.

--- app/src/main/java/edu/ucne/kias_rent_car/presentation/AdminTareas/AdminUsuarios/AdminUsuariosUiState.kt
+++ app/src/main/java/edu/ucne/kias_rent_car/presentation/AdminTareas/AdminUsuarios/AdminUsuariosUiState.kt
@@ -5,6 +5,5 @@ import edu.ucne.kias_rent_car.domain.model.Usuario
 data class AdminUsuariosUiState(
     val usuarios: List<Usuario> = emptyList(),
     val searchQuery: String = "",
-    val isLoading: Boolean = false,
-    val error: String? = null
+    val isLoading: Boolean = false
 )
